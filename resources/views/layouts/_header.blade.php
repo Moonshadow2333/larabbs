4c1cd1cd141692f38fed95bcecd15c182636366c
moonshadow <1815265375@qq.com>
2022-02-03 04:18:16 +0000

    基础页面结构

---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light navbar-static-top">
+  <div class="container">
+    <a class="navbar-brand" href="#">LaraBBS</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+      <!-- Left Side Of Navbar -->
+      <ul class="navbar-nav mr-auto">
+
+      </ul>
+
+      <!-- Right Side Of Navbar -->
+      <ul class="navbar-nav navbar-right">
+        <li class="nav-item">
+          <a class="nav-link" href="#">登录</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">注册</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{str_replace('_','-',app()->getLocale())}}">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{csrf_token()}}">
+
+  <title>@yield('title','Larabbs') - laravel进阶教程</title>
+  <link rel="stylesheet" type="text/css" href="{{mix('css/app.css')}}">
+  <link rel="stylesheet" type="text/css" href="/css/mycss.css">
+</head>
+<body>
+  <div class="{{route_class()}}-page" id="app">
+    @include('layouts._header')
+    <div class="container">
+      @include('shared._messages')
+      <!-- 占块区域 -->
+      @yield('content')
+    </div>
+    @include('layouts._footer')
+  </div>
+  <!-- Script -->
+  <script src="{{mix('js/app.js')}}"></script>
+</body>
+</html>
