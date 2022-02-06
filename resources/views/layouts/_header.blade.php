6e16bc0485517f7800dbe83c2a30a09f6ab6f253
moonshadow <1815265375@qq.com>
2022-02-06 04:36:33 +0000

    修复跳转链接

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -12,12 +12,35 @@
 
       <!-- Right Side Of Navbar -->
       <ul class="navbar-nav navbar-right">
+        @guest
         <li class="nav-item">
           <a class="nav-link" href="{{route('login')}}">登录</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="{{route('register')}}">注册</a>
         </li>
+        @else
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" id="navbarDropdown" role="button">
+              <img src="https://cdn.learnku.com/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60" class="img-responsive img-circle" width="30px" height="30px">
+              {{Auth::user()->name}}
+            </a>
+            <div class="dropdown-menu">
+              <a class="dropdown-item" href="#">个人中心</a>
+              <a class="dropdown-item" href="#">编辑资料</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#">
+                <form action="{{route('logout')}}" method="POST">
+                  {{csrf_field()}}
+                  <button type="submit" class="btn btn-block btn-danger" name="button">
+                    退出
+                  </button>
+                </form>
+              </a>
+            </div>
+          </li>
+
+        @endguest
       </ul>
     </div>
   </div>
