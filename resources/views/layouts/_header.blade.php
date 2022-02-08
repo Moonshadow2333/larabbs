2b43f9f94a15b141f96f041fb0f68f61e7f02375
moonshadow <1815265375@qq.com>
2022-02-08 02:35:49 +0000

    生成话题骨架

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -22,7 +22,7 @@
         @else
         <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" id="navbarDropdown" role="button">
-              <img src="{{$user->avatar}}" class="img-responsive img-circle" width="30px" height="30px">
+              <img src="{{Auth::user()->avatar}}" class="img-responsive img-circle" width="30px" height="30px">
               {{Auth::user()->name}}
             </a>
             <div class="dropdown-menu">
