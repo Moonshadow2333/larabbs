63ab311e7a41bb9f25c594d59f438b4f5efe080e
moonshadow <1815265375@qq.com>
2022-02-08 07:06:17 +0000

    话题列表页面

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -9,4 +9,11 @@ class Topic extends Model
     use HasFactory;
     
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+
+    public function category(){
+        return $this->belongsTo(category::class);
+    }
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
