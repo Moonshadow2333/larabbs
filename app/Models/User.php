137994d64ac27c08e09cd55e12ca964023237930
moonshadow <1815265375@qq.com>
2022-02-14 09:13:45 +0000

    用户管理后台

--- app/Models/User.php
+++ app/Models/User.php
@@ -76,4 +76,20 @@ class User extends Authenticatable implements MustVerifyEmailContract
         $this->save();
         $this->unreadNotifications->markAsRead();
     }
+    public function setPasswordAttribute($value){
+        // 如果长度等于60，即认为是已近做过加密的情况
+        if(strlen($value) != 60){
+            // 不等于60， 做密码加密处理
+            $value = bcrypt($value);
+        }
+        $this->attributes['password'] = $value;
+    }
+    public function setAvatarAttribute($path){
+        // 如果不是http子串开头，那就是从后台上传的，需要补全URL
+        if(!\Str::startsWith($path,'http')){
+            // 拼接完整的URL
+            $path = config('app.url')."/uploads/images/avatar/$path";
+        }
+        $this->attributes['avatar'] = $path;
+    }
 }
